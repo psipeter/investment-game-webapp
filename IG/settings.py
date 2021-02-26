2c186491522775e9527bf0b10e1f9eccb7715247
Andreas Stöckel <astoecke@uwaterloo.ca>
2021-02-25 22:25:56 -0500

    Add CSP header

--- IG/settings.py
+++ IG/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'game.custom_headers.CSPMiddleware',
 ]
 
 ROOT_URLCONF = 'IG.urls'
