7ee17b330cd02434c70faa06d8a381cd0f343b01
Peter Duggins <psipeter@gmail.com>
2021-05-09 09:23:15 -0400

    add seed to admin view

--- game/admin.py
+++ game/admin.py
@@ -6,7 +6,7 @@ from .models import Game, Agent, User, Blob, Feedback
 # admin.site.register(Game)
 @admin.register(Game)
 class Game(admin.ModelAdmin):
-	list_display = ('uuid', 'user', 'get_userGroup', 'userRole', 'agentRole', 'tStart', 'tEnd', 'userGives', 'userKeeps', 'userRewards', 'userTimes', 'agentGives', 'agentKeeps', 'agentRewards')
+	list_display = ('uuid', 'user', 'get_userGroup', 'userRole', 'agentRole', 'seed', 'tStart', 'tEnd', 'userGives', 'userKeeps', 'userRewards', 'userTimes', 'agentGives', 'agentKeeps', 'agentRewards')
 	ordering = ('-tStart',)
 	def get_userGroup(self, obj):
 		return obj.agent.userGroup  # displays 'tutorial'
